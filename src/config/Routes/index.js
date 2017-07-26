63160d6734d13bd3b8da06cbc4860b825aac7ea8
Mike Eastes <michael.eastes@gmail.com>
2017-07-26 07:00:27 -0500

    Adds redirect to catalog for `/market`.

--- src/config/Routes/index.js
+++ src/config/Routes/index.js
@@ -8,6 +8,7 @@ import ThankYou from '../../scenes/Market/ThankYou';
 export default () =>
   (<div>
     <Route exact path="/" render={() => <Redirect to="/market/catalog" />} />
+    <Route exact path="/market" render={() => <Redirect to="/market/catalog" />} />
     <Route path="/market/catalog" component={Catalog} />
     <Route path="/market/shipping" component={ShippingForm} />
     <Route path="/market/thanks" component={ThankYou} />
